46adbcf87b132b8c130dea5323bd561ba0b7610c
Madhumitha Anandan <mmithaa@users.noreply.github.com>
2017-06-01 14:00:12 -0400

    Add bounds related .py files

---
+++ Edit bounding box files/Copy_det_faces_now.py
@@ -0,0 +1,92 @@
+import os,sys
+import glob
+import cv2
+import PIL
+import numpy as np
+import pandas as pd
+from skimage import io
+
+#import matplotlib
+#matplotlib.use('Agg')
+
+#%matplotlib inline  
+import matplotlib.pyplot as plt
+from PIL import Image
+from scipy.misc import toimage
+from scipy.signal import medfilt
+from scipy.ndimage.filters import median_filter
+#%matplotlib inline 
+import shutil as sh
+
+faces_file = '/data0/madhu/Dataset/Raw Videos/Images/Faces/'
+bounds_file = '/data0/madhu/Dataset/Training/Bounds/'
+faces_out = '/data0/madhu/Dataset/Raw Videos/Images/Faces_fin/'
+bounds_out = '/data0/madhu/Dataset/Training/Bounds_fin/'
+dirs_faces = sorted(os.walk(faces_file).next()[1])
+dirs_bounds = sorted(os.walk(bounds_file).next()[2])
+
+#Output diectory to store FINAL FACES
+directory = os.path.dirname(faces_out)
+#print ('directory=',directory)
+#Delete folder to clear its contents
+if os.path.exists(directory):
+    sh.rmtree(directory)
+#Create directory if not present
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+#Output diectory to store dir
+directory = os.path.dirname(bounds_out)
+#print ('directory=',directory)
+#Delete folder to clear its contents
+if os.path.exists(directory):
+    sh.rmtree(directory)
+#Create directory if not present
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+
+ctr = -1
+copy_array2 = []
+#Iterate across bounds files (counter)
+for i_fil in range(np.size(dirs_bounds)):
+
+    #Output diectory to store FINAL FACES
+    directory = os.path.dirname(faces_out+dirs_bounds[i_fil]+'/')
+    #print ('directory=',directory)
+    #Delete folder to clear its contents
+    if os.path.exists(directory):
+    	sh.rmtree(directory)
+    #Create directory if not present
+    if not os.path.exists(directory):
+	os.makedirs(directory)
+
+    #Iterate across bounds files (actual)
+    for ffilename in sorted(glob.glob(bounds_file+dirs_bounds[i_fil])):
+	copy_array = []
+        print (ffilename)
+        del_array = []
+        dataa = np.loadtxt(ffilename)
+
+        #Check which image i.e., row has all 0 bounds and append the imdex to a numpy array
+        for i in range (np.shape(dataa)[0]):
+            if (dataa[i]).all() == np.array([0, 0, 0, 0]).all():
+                del_array.append(i)
+            else:
+            #Copy corresponding values to a new text file
+                ctr = ctr+1
+                copy_array2.append(dataa[i])
+		copy_array.append(dataa[i])
+                   
+                
+                #Iterate across images in a folder
+                #for ifilename in sorted(glob.glob(faces_file+dirs_bounds[i_fil]+'/*.jpg')): #assuming jpg
+                img = cv2.imread(faces_file+dirs_bounds[i_fil]+'/out-'+str(i+1).zfill(6)+'.jpg')
+                cv2.imwrite(faces_out+dirs_bounds[i_fil]+'/'+str(ctr).zfill(7)+'.jpg', img)
+		print (faces_out+dirs_bounds[i_fil]+'/'+str(ctr).zfill(7)+'.jpg')
+
+    	#Write non-zero bounds to a file
+    	np.savetxt(bounds_out+dirs_bounds[i_fil], copy_array) 
+            
+#Write non-zero bounds to a file
+np.savetxt(bounds_out+'Bounds_fin.txt', copy_array2)           
---
+++ Edit bounding box files/Save_boxes_fin.py
@@ -0,0 +1,88 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+import os,sys
+import glob
+import cv2
+import PIL
+import numpy as np
+import pandas as pd
+from skimage import io
+
+#import matplotlib
+#matplotlib.use('Agg')
+
+#get_ipython().magic(u'matplotlib inline')
+import matplotlib.pyplot as plt
+from PIL import Image
+from scipy.misc import toimage
+from scipy.signal import medfilt
+from scipy.ndimage.filters import median_filter
+#get_ipython().magic(u'matplotlib inline')
+import shutil as sh
+
+
+if __name__ == "__main__":
+    # List of all files with median filtered bounding box coordinates
+    tmpf = '/data0/madhu/Dataset/Training/Bounds_fin/'
+    tmpi = '/data0/madhu/Dataset/Raw Videos/Images/Faces_fin/'
+    tmpi2 = '/data0/madhu/Dataset/Raw Videos/Images/Boxes_fin/'
+    dirs_bounds = sorted(os.walk(tmpf).next()[2])
+    # List of all folders with images
+    dirs_images = sorted(os.walk(tmpi).next()[1])
+    dirs_boxes = sorted(os.walk(tmpi2).next()[1])
+    
+    
+    #Iterate across text files and images
+    for i_fil in range(np.size(dirs_images)):
+        
+        #Output diectory to store the box images
+        directory = os.path.dirname(tmpi2+dirs_images[i_fil]+'/')
+        #Clear contents of folder
+        if os.path.exists(directory):
+            sh.rmtree(directory)
+        #Create directory if not present
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        for ffilename in sorted(glob.glob(tmpf+dirs_images[i_fil])): #assuming jpg
+            print (dirs_images[i_fil])
+            #if dirs_bounds[i_fil]=='GOPR0854':
+            #continue
+            #x='data'+str(i_fil)   
+            #vars()[x] = np.loadtxt(ffilename)
+            data = np.loadtxt(ffilename)
+            
+            ctr = -1
+            ctr2 = 0
+                
+            for ifilename in sorted(glob.glob(tmpi+dirs_images[i_fil]+'/*.jpg')): #assuming jpg
+                #print (ifilename)
+                ctr = ctr+1
+                ctr2 = ctr2+1
+                #if dirs_bounds[i_fil] == 'GOPR0863':
+                if ctr2 == 400:
+                    print (ctr)
+                    ctr2 = 0
+                    #if ctr<=20150:
+                        #continue
+                    #if ctr>=20170:
+                        #raw_input("Press Enter to continue...")
+                imm = cv2.imread(ifilename)
+                #print (imm)
+                #h = vars()[x]
+                left = int(data[ctr, 0])
+                top = int(data[ctr, 1])
+                right = int(data[ctr, 2])
+                bottom = int(data[ctr, 3])
+                cv2.rectangle(imm,(left,top),(right,bottom),(0,255,0),3)
+                file_path = tmpi2+dirs_images[i_fil]+'/box'+str(ctr).zfill(8)+'.jpg'
+                #plt.imshow(imm)
+                #plt.savefig(file_path)
+                #plt.close()
+		cv2.imwrite(file_path, imm)
+                #Image.open(file_path3).save('testplot.jpg','JPEG')
+
+
